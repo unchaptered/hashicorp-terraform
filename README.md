adcc6e69a30e2d495f2e61a0d66c13d3bc8ec55d
unchaptered <workstation19961002@gmail.com>
2023-03-08 10:51:17 +0900

    Terraform : ec2 instance with default VPC

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # terraform
+
+`terraform init`을 사용하면 다음의 경로에 Infra Provider와 소통하기 위한 API가 다운로드 됩니다.
+
+```cmd
+~\.terraform\providers\registry.terraform.io\hashicorp\aws\4.57.1\windows_386\terraform-provider-aws_v4.57.1_x5.exe
+```
---
+++ ec2-instance.tf
@@ -0,0 +1,23 @@
+# Provider Block
+provider "aws" {
+    region = "ap-northeast-2"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+    ami             = "ami-030e520ec063f6467"
+    instance_type   = "t2.micro"
+    tags = {
+      "Name" = "ec2demo"
+      
+      "resource-level": "vpc"
+      "resource-region": "ap-northeast-2"
+
+      "group-stage": "terraform-learn"
+      "group-service": "terraform-learn"
+      "group-purpose": "api-server"
+      "group-position": "backend"
+
+      "manager": "unchaptered"
+    }
+}
