1afe54563746bc37019b0b625c2c33410494709a
unchaptered <workstation19961002@gmail.com>
2023-03-10 09:38:57 +0900

    Terraform : List, Map syntax for 05-Terraform-Loop-MetaArguments-SplatOperator

--- 05-Terraform-Loops-MetaArguments-SplatOperator/c2-variables.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/c2-variables.tf
@@ -17,3 +17,25 @@ variable "instance_key_pair" {
     type        = string
     default     = "terraform-key"
 }
+
+
+# List Variabes
+variable "instance_type_list" {
+    description = "Instance Type List"
+    type        = list(string)
+
+    default     = [ "t3.micro", "t3.small" ]
+}
+
+
+# Map Variables
+variable "instance_type_map" {
+    description = "EC2 Instance Type"
+    type        = map(string)
+    
+    default = {
+        "dev"   = "t3.micro"
+        "qa"    = "t3.small"
+        "prod"  = "t3.large"
+    }
+}
