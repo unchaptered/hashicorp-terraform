242b6e8543d27b2fe1c92912eb1c18d64c98ad56
unchaptered <workstation19961002@gmail.com>
2023-03-10 13:39:52 +0900

    Terraform : for_each syntax for 05-Terraform-Loops-MetaArguments-SplatOperator

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/c6-outputs.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-02-MetaArgument-for_each/c6-outputs.tf
@@ -2,47 +2,20 @@
 
 output "instance_public_ip" {
     description = "EC2 Instance Public IP"
-    value       = aws_instance.ec2demo[*].public_ip
+    # value       = aws_instance.ec2demo.*.public_ip      # Legacy Splat
+    # value       = aws_instance.ec2demo[*].public_ip     # Latest Splat
+    value       = toset([for instance in aws_instance.ec2demo: instance.public_ip])
 }
 
 output "instance_public_dns" {
     description = "EC2 Instance Public IP"
-    value       = aws_instance.ec2demo[*].public_dns
-}
-
-# For Loop를 사용한 list 생성
-output "for_output_list" {
-    description = "For Loop with List"
-    value       = [for instance in aws_instance.ec2demo: instance.public_dns]
+    # value       = aws_instance.ec2demo.*.public_dns         # Legacy Splat
+    # value       = aws_instance.ec2demo[*].public_dns        # Latest Splat
+    value       = toset([for instance in aws_instance.ec2demo: instance.public_ip])
 }
 
 
-# For Loop를 사용한 Map 생성
-output "for_output_map" {
-    description = "For Loop with List"
-    value       = {
-        for instance in aws_instance.ec2demo: instance.id => instance.public_dns
-    }
-}
 
-# For Loop를 사용한 Advanced Map 생성
-output "for_output_map_2" {
-    description = "For Loop with List"
-    value       = {
-        for count, instance in aws_instance.ec2demo: count => instance.public_dns
-    }
-}
-
-
-# Legacy Splat(*) Opertaor
-output "legacy_splat_operator_public_dns" {
-    description = "Legacy Splat Operator"
-    value       = aws_instance.ec2demo.*.public_dns
-}
-
-
-# Splat(*) Opertaor
-output "splat_operator_public_dns" {
-    description = "Splat Operator"
-    value       = aws_instance.ec2demo[*].public_dns
+output "instance_public_dns2" {
+    value       = tomap({ for az, instance in aws_instance.ec2demo: az => instance.public_dns })
 }
