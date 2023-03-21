dbb5440973b58045f1dd5e7418a36a13fe04a67d
unchaptered <workstation19961002@gmail.com>
2023-03-22 00:13:32 +0900

    Terraform : <Resource Block> for Elastip IP of Bastion Host for 07-AWS_EC2Instance-and-SecurityGroups

--- 07-AWS-EC2Instance-and-SecurityGroups/c7-02-ec2instance-outputs.tf
+++ 07-AWS-EC2Instance-and-SecurityGroups/c7-02-ec2instance-outputs.tf
@@ -5,12 +5,12 @@
 ## ec2_bastion_public_ip
 output "ec2_bastion_public_instance_ids" {
     description = "List of IDs of instances"
-    value       = module.ec2_public[*].id
+    value       = module.ec2_public.id
 }
 
 output "ec2_bastion_public_ip" {
     description = "List of public IP addresses assigned to the instances"
-    value       = module.ec2_public[*].public_ip
+    value       = module.ec2_public.public_ip
 }
 
 
