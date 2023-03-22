c95a7a13a91615f5a62c82832e2f144aee4c74b9
unchaptered <workstation19961002@gmail.com>
2023-03-22 20:14:34 +0900

    Terraform : <Null Resource Block> for automation for 07-AWS_EC2Instance-and-SecurityGroups

--- 07-AWS-EC2Instance-and-SecurityGroups/c1-versions.tf
+++ 07-AWS-EC2Instance-and-SecurityGroups/c1-versions.tf
@@ -6,10 +6,12 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.20"
     }
-    # null = {
-    #   source = "hashicorp/null"
-    #   version = "~> 3.0"
-    # }        
+
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+    }        
+    
   }
 }
 
