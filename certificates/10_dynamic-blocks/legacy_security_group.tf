c189e26c16876fe2141d9eed4f0ade68e38bd4e3
unchaptered <workstation19961002@gmail.com>
2023-08-24 19:19:19 +0900

    Create : Dynamic Blocks for Certificates

---
+++ certificates/10_dynamic-blocks/legacy_security_group.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "legacy_security_group" {
+
+  name = "sample-sg"
+
+  ingress {
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8201
+    to_port     = 8201
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8300
+    to_port     = 8300
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 9500
+    to_port     = 9500
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
