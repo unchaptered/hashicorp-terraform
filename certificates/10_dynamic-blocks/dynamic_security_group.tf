44faa93249d5157d8656a7ff26f6ba18af78ef09
unchaptered <workstation19961002@gmail.com>
2023-08-24 19:23:46 +0900

    Update : Dynamic Block with Iterator(Optional) for Certificates

--- certificates/10_dynamic-blocks/dynamic_security_group.tf
+++ certificates/10_dynamic-blocks/dynamic_security_group.tf
@@ -8,11 +8,25 @@ resource "aws_security_group" "dynamic_security_group" {
   name        = "dynamic-sg"
   description = "Ingress for Vault"
 
+  # Dynamic Blocks without Iterator
   dynamic "ingress" {
     for_each = var.sg_ports
     content {
       from_port = ingress.value
       to_port   = ingress.value
+
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  # Dynamic Block with Iterator
+  dynamic "egress" {
+    for_each = var.sg_ports
+    iterator = port
+    content {
+      from_port = port.value
+      to_port   = port.value
 
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
