9a1329919546c3ae9529f1403f8d0288320cf2c0
unchaptered <workstation19961002@gmail.com>
2023-08-24 17:37:31 +0900

    Create : Conditional Expression for Certificates

---
+++ certificates/07_conditional_expression/ec2_instances.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "dev" {
+  ami           = "ami-0c9c942bd7bf113a2"
+  instance_type = "t2.micro"
+
+  count = var.istest == true ? 1 : 0
+}
+
+resource "aws_instance" "prod" {
+  ami           = "ami-0c9c942bd7bf113a2"
+  instance_type = "t2.large"
+
+  count = var.istest == false ? 1 : 0
+}
