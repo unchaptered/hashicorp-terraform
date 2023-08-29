23c2ed9bef906208d3859c90baf7eb82db95087d
unchaptered <workstation19961002@gmail.com>
2023-08-29 17:30:33 +0900

    Create : Security Challenge

---
+++ certificates/21_security-challenge/rds.tf
@@ -0,0 +1,17 @@
+resource "aws_db_instance" "default" {
+  allocated_storage = 5
+  storage_type      = "gp2"
+
+  engine         = "mysql"
+  engine_version = "5.7"
+
+  instance_class = "db.t2.micro"
+
+  identifier = "mydbaasds"
+  username   = "foo"
+
+  password = file("./rds_password.txt")
+
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = "true"
+}
